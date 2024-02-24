0e809236d0841f2cb1f327c5dfda16b1e1d29039
Md Aftab Ansari <ansariaftab200000@gmail.com>
2024-02-24 12:01:13 +0530

    Added react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'  
+ // component imorts 
+ import App from './App.jsx'
+ // CSS imports 
+ import './index.css'  
+ // Labriry imports 
+ import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- 
+ ReactDOM.createRoot(document.getElementById('root')).render(
+ <BrowserRouter>
     <App />
-);
+   <Toaster />
+    </BrowserRouter>
+ );
